479727bf4d079a06f5509adb55e01d91bd56ec84
mrzcs <mrzcs@163.com>
2018-09-11 17:40:39 +0800

    update ch03

--- ch03-btree/trees.py
+++ ch03-btree/trees.py
@@ -2,6 +2,7 @@
 from math import log
 import operator
 from imp import reload # to enable reload in py3
+import matplotlib.pyplot as plt
 
 def calcShannonEnt(dataSet):
     """
@@ -150,14 +151,14 @@ Returns:
 
 def createTree(dataSet, labels):
     classList = [example[-1] for example in dataSet]
-    print(classList)
+    #print(classList)
     if classList.count(classList[0]) == len(classList): # stop when all classes are equal
-        print("all classes are equal")
+        #print("all classes are equal")
         return classList[0]
     if len(dataSet[0]) == 1: # when no more features, return majority
-        print("no more features")
+        #print("no more features")
         return majorityCnt(classList)
-    print("main")
+    #print("main")
     bestFeat = chooseBestFeature(dataSet)
     bestFeatLabel = labels[bestFeat]
     #featLabels.append(bestFeatLabel)
@@ -170,8 +171,83 @@ def createTree(dataSet, labels):
         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
                            
     return myTree  
+    
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = next(iter(myTree))
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs +=1 
+    return numLeafs
 
-     
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = next(iter(myTree))
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    return maxDepth
+   
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    arrow_args = dict(arrowstyle="<-")
+    createPlot.ax1.annotate(nodeTxt, 
+                    xy=parentPt, 
+                    xycoords='axes fraction', 
+                    xytext=centerPt, 
+                    textcoords='axes fraction',
+                    va="center", 
+                    ha="center", 
+                    bbox=nodeType, 
+                    arrowprops=arrow_args)
+                    
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+    leafNode = dict(boxstyle="round4", fc="0.8")
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = next(iter(myTree))
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree. yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+        
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW =  float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5 / plotTree.totalW
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.show()
+
+
+
+        
 
 
 
